e6ec489a04430933335c2c656feabb8a6669ee72
Daniel Patru <dpatru@gmail.com>
2016-12-20 21:12:28 -0500

    Add coment on how to add autocompletion.

--- _now.sh
+++ _now.sh
@@ -1,7 +1,9 @@
-
 #!env bash
 # set -x
 # run to enable autocompletion.
+# Put the following line in your .profile or .bashrc file to enable
+# autocompletion for now:
+#         . path/to/_now.sh
 
 _now() 
 {
