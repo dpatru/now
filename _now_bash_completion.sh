7bf0b483e3037f08bbec4d707f135e404ab3eb12
Daniel Patru <dpatru@gmail.com>
2017-01-07 21:44:56 -0500

    Remove old files.
    These are superseded by the rewrite of now in perl.

--- _now_bash_completion.sh
+++
@@ -1,8 +0,0 @@
-#!env bash
-# set -x
-# run to enable autocompletion.
-# Put the following line in your .profile or .bashrc file to enable
-# autocompletion for now:
-#         . path/to/_now_bash_completion.sh
-
-complete -C now now
